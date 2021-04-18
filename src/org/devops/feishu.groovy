133fbaa8db15ab30659df128999be7166b4cf56e
notevery <494919572@qq.com>
2021-04-18 13:51:38 +0800

    Create feishu.groovy

---
+++ src/org/devops/feishu.groovy
@@ -0,0 +1,33 @@
+package org.devops
+
+def FeiShuHTTPRequest(message,href_url,feishuUrl){
+      result = httpRequest httpMode: "POST", 
+                consoleLogResponseBody: true,
+                ignoreSslErrors: true, 
+                requestBody: '{
+            "msg_type": "post",
+            "content": {
+                "post": {
+                    "zh_cn": {
+                        "title": "流水线状态通知",
+                        "content": [
+                            [
+                                {
+                                    "tag": "text",
+                                    "text": "${env.REPO_NAME} 项目流水线有更新: "
+                                },
+                                {
+                                    "tag": "a",
+                                    "text": message,
+                                    "href": href_url
+                                }
+                            ]
+                        ]
+                    }
+                }
+            }
+        }',
+                url: feishuUrl
+                //quiet: true
+    return result
+}
